57f99e5a80fde5b93c3a3824f6faaf586e76018a
arseniysij <50396694+arseniysij@users.noreply.github.com>
2020-06-22 14:25:31 +0500

    sesktop

--- js/_custom.js
+++ js/_custom.js
@@ -1,5 +1,48 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-	// Custom JS
+	
 
+});
+
+$(function() {
+	let map = $('.location-map').hide();
+	let link = $('.location-text__map-link').hover(function(){
+		$(map).fadeIn(150);
+	}, function() {
+		if ($(map, link).mouseout(function(){
+			$(map).fadeOut(200);
+		}));
+	});
+
+	var rellax = new Rellax('.rellax');
+
+	$('.popup__select').select2({
+		minimumResultsForSearch: -1, 
+	});
+
+	// popup
+	let popup = $("#popup");
+	let signUpBtn = $('#signup');
+	let clientCardBtn = $('#hero__btn');
+	let popupClose = $('.popup-close');
+	popup.hide();
+	$(signUpBtn).click(function(){
+		popup.fadeIn(200);
+		return false;
+	}); 
+	popupClose.click(function() {
+		popup.fadeOut(200);
+	});
+	clientCardBtn.click(function() {
+		popup.fadeIn(200);
+		return false;
+	});
+	$(document).click(function(event) { 
+  $target = $(event.target);
+  if(!$target.closest(popup).length && 
+  $(popup).is(":visible")) {
+    $(popup).fadeOut(200);
+  }        
+});
+	
 });
